7b7313f2b8b0d48cb249a3714ecf5835a6127dd0
Kanersps <kanepetra@gmail.com>
2021-03-18 21:29:21 +0100

    tweak: change application statistic requests to method

--- glytics.Common/Models/Applications/Application.cs
+++ glytics.Common/Models/Applications/Application.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 
 namespace glytics.Common.Models.Applications
 {
@@ -21,5 +23,70 @@ namespace glytics.Common.Models.Applications
         public List<ApplicationStatistic> Statistic { get; set; }
         public List<ApplicationStatisticPath> PathStatistic { get; set; }
         public List<ApplicationStatisticBrowser> BrowserStatistic { get; set; }
+
+        private List<ApplicationStatistic> GetGeneralStatisticsByRange(long[] range)
+        {
+            return Statistic.OrderByDescending(stat => stat.Timestamp).Where(stat =>
+                stat.Timestamp > DateTimeOffset.FromUnixTimeMilliseconds(range[0]) &&
+                stat.Timestamp < DateTimeOffset.FromUnixTimeMilliseconds(range[1])).ToList();
+        }
+
+        private List<ApplicationStatisticPath> GetPathStatisticsByRange(long[] range)
+        {
+            return PathStatistic.OrderByDescending(stat => stat.Timestamp).Where(stat =>
+                stat.Timestamp > DateTimeOffset.FromUnixTimeMilliseconds(range[0]) &&
+                stat.Timestamp < DateTimeOffset.FromUnixTimeMilliseconds(range[1])).ToList();
+        }
+
+        private List<ApplicationStatisticBrowser> GetBrowserStatisticsByRange(long[] range)
+        {
+            return BrowserStatistic.OrderByDescending(stat => stat.Timestamp).Where(stat =>
+                stat.Timestamp > DateTimeOffset.FromUnixTimeMilliseconds(range[0]) &&
+                stat.Timestamp < DateTimeOffset.FromUnixTimeMilliseconds(range[1])).ToList();
+        }
+
+        #nullable enable
+        public WebsiteDetails GetDetails(long[]? range)
+        {
+            if(range == null)
+                range = new []{ DateTimeOffset.UtcNow.AddDays(-30).ToUnixTimeMilliseconds(), DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() };
+
+            List<long[]> hourly = new List<long[]>();
+            List<dynamic[]> hourlyPath = new List<dynamic[]>();
+            List<dynamic[]> hourlyBrowser = new List<dynamic[]>();
+            
+            foreach (ApplicationStatistic stat in GetGeneralStatisticsByRange(range))
+            {
+                hourly.Add(new[]
+                    {((DateTimeOffset) stat.Timestamp).ToUnixTimeMilliseconds(), stat.Visits, stat.PageViews});
+            }
+
+            foreach (ApplicationStatisticPath stat in GetPathStatisticsByRange(range))
+            {
+                hourlyPath.Add(new dynamic[]
+                {
+                    ((DateTimeOffset) stat.Timestamp).ToUnixTimeMilliseconds(), stat.Visits, stat.PageViews,
+                    stat.Path
+                });
+            }
+
+            foreach (ApplicationStatisticBrowser stat in GetBrowserStatisticsByRange(range))
+            {
+                hourlyBrowser.Add(new dynamic[]
+                {
+                    ((DateTimeOffset) stat.Timestamp).ToUnixTimeMilliseconds(), stat.Visits, stat.PageViews,
+                    stat.Browser
+                });
+            }
+
+            return new WebsiteDetails()
+            {
+                Address = $"https://{Address}",
+                Name = Name,
+                Hourly = hourly,
+                HourlyPaths = hourlyPath,
+                HourlyBrowsers = hourlyBrowser
+            };
+        }
     }
 }
--- glytics.Logic/Application/ApplicationService.cs
+++ glytics.Logic/Application/ApplicationService.cs
@@ -46,60 +46,7 @@ namespace glytics.Logic.Application
         {
             Common.Models.Applications.Application web = _unitOfWork.Application.GetDetails(trackingCode);
 
-            var hourly = new List<long[]>();
-            var hourlyPath = new List<dynamic[]>();
-            var hourlyBrowser = new List<dynamic[]>();
-
-            if (web != null && web.Statistic.Count > 0)
-            {
-                List<ApplicationStatistic> period =
-                    web.Statistic.OrderByDescending(stat => stat.Timestamp).Where(stat =>
-                        stat.Timestamp > DateTimeOffset.FromUnixTimeMilliseconds(curRange[0]) &&
-                        stat.Timestamp < DateTimeOffset.FromUnixTimeMilliseconds(curRange[1])).ToList();
-
-                List<ApplicationStatisticPath> lastMonthPaths =
-                    web.PathStatistic.OrderByDescending(stat => stat.Timestamp).Where(stat =>
-                        stat.Timestamp > DateTimeOffset.FromUnixTimeMilliseconds(curRange[0]) &&
-                        stat.Timestamp < DateTimeOffset.FromUnixTimeMilliseconds(curRange[1])).ToList();
-
-                List<ApplicationStatisticBrowser> lastMonthBrowsers =
-                    web.BrowserStatistic.OrderByDescending(stat => stat.Timestamp).Where(stat =>
-                        stat.Timestamp > DateTimeOffset.FromUnixTimeMilliseconds(curRange[0]) &&
-                        stat.Timestamp < DateTimeOffset.FromUnixTimeMilliseconds(curRange[1])).ToList();
-
-                foreach (ApplicationStatistic stat in period)
-                {
-                    hourly.Add(new[]
-                        {((DateTimeOffset) stat.Timestamp).ToUnixTimeMilliseconds(), stat.Visits, stat.PageViews});
-                }
-
-                foreach (ApplicationStatisticPath stat in lastMonthPaths)
-                {
-                    hourlyPath.Add(new dynamic[]
-                    {
-                        ((DateTimeOffset) stat.Timestamp).ToUnixTimeMilliseconds(), stat.Visits, stat.PageViews,
-                        stat.Path
-                    });
-                }
-
-                foreach (ApplicationStatisticBrowser stat in lastMonthBrowsers)
-                {
-                    hourlyBrowser.Add(new dynamic[]
-                    {
-                        ((DateTimeOffset) stat.Timestamp).ToUnixTimeMilliseconds(), stat.Visits, stat.PageViews,
-                        stat.Browser
-                    });
-                }
-            }
-
-            return new WebsiteDetails()
-            {
-                Address = $"https://{web.Address}",
-                Name = web.Name,
-                Hourly = hourly,
-                HourlyPaths = hourlyPath,
-                HourlyBrowsers = hourlyBrowser
-            };
+            return web.GetDetails(curRange);
         }
 
         public async Task<SimpleWebsiteDetails> GetWebsite(TrackingCode trackingCode)
